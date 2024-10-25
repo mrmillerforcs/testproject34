ca58cf7bceb47ad688416bb84dee97d65315768f
Quincy Holmer <qholmer@schools.nyc.gov>
2024-10-25 09:49:32 -0400

    added print statements

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,5 +1,8 @@
 public class TestRunner {
     public static void main(String[] args) {
         System.out.println("hello!");
+        System.out.println("this is fun");
+        System.out.println("welcome");
+        System.out.println("arndom");
     }
 }
