01cdc239b81775997f9571659aea18ed77cb0f1f
Quincy Holmer <qholmer@schools.nyc.gov>
2024-10-25 09:50:12 -0400

    added stuff

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,8 +1,9 @@
 public class TestRunner {
     public static void main(String[] args) {
         System.out.println("hello!");
-        System.out.println("this is fun");
+        System.out.println("this is awesome");
         System.out.println("welcome");
         System.out.println("arndom");
+        System.out.println("hello again");
     }
 }
